ea3eb99254866da6dc172213514674fb8bfab7ef
MintyU <mintyu.dev@gmail.com>
2025-12-05 14:29:52 +0900

    Lecture 13

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts/{postId}/comments")
@@ -47,7 +49,7 @@ public class ApiV1PostCommentController {
 
     @Transactional
     @GetMapping("/{id}/delete")
-    public String deleteItem(
+    public Map<String, Object> deleteItem(
             @PathVariable int postId,
             @PathVariable int id
     ) {
@@ -56,6 +58,10 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return "%d번 댓글이 삭제되었습니다.".formatted(id);
+        Map<String, Object> rsData = new LinkedHashMap<>();
+        rsData.put("resultCode", "200-1");
+        rsData.put("msg", "%d번 댓글이 삭제되었습니다.".formatted(postComment.getId()));
+
+        return rsData;
     }
 }
