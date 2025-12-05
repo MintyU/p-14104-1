7c4008217a75b10411b8eff1707cbdf45595a051
MintyU <mintyu.dev@gmail.com>
2025-12-05 15:21:50 +0900

    Lecture 16

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,6 +3,8 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
+import com.back.global.rsData.ForPostRsData;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,5 +37,19 @@ public class ApiV1PostController {
         Post post = postService.findById(id).get();
 
         return new PostDto(post);
+    }
+
+    @Transactional
+    @GetMapping("/{id}/delete")
+    public ForPostRsData delete(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+
+        postService.delete(post);
+
+        return new ForPostRsData(
+                "200-1",
+                "%d번 글이 삭제되었습니다.".formatted(id),
+                new PostDto(post)
+        );
     }
 }
